5ec3b950d2c01cabe423fd59f9728e46c0a564de
daniela oliveira <danioliver_17@hotmail.com>
2022-11-14 14:17:14 -0300

    Uso do Redux

--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -4,20 +4,33 @@ import Postagem from '../../../models/Postagem';
 import { busca } from '../../../services/Service'
 import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaPostagem.css';
-import useLocalStorage from 'react-use-localstorage';
 import { useNavigate } from 'react-router-dom'
 import { Box } from '@mui/material';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 
 
 function ListaPostagem() {
   const [posts, setPosts] = useState<Postagem[]>([])
-  const [token, setToken] = useLocalStorage('token');
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+);
   let navigate = useNavigate();
 
   useEffect(() => {
     if (token == "") {
-      alert("Você precisa estar logado")
+      toast.error('Você precisa estar logado!',{
+        position:'top-right',
+        autoClose:2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        theme: "colored",
+        progress: undefined,        
+    });
       navigate("/login")
 
     }
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,33 +1,62 @@
-import React from 'react';
-import { Typography, Grid, Button } from '@material-ui/core';
-import {Box} from '@mui/material'
-import './Home.css';
+import {Box} from '@mui/material';
+import {Typography,Grid, Button} from '@material-ui/core';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+import './Home.css';
+import { useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../store/tokens/tokensReducer';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 function Home() {
-  return (
-      <>
-          <Grid container direction="row" justifyContent="center" alignItems="center"className="caixa" >
-              <Grid alignItems="center" item xs={6}>
-                  <Box paddingX={20} >
-                      <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Estou feliz em ter você aqui!</Typography>
-                      <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
-                  </Box>
-                  <Box display="flex" justifyContent="center">
-                      <Box marginRight={1}>
-                      </Box>
-                      <Button variant="outlined" className="botao">Da uma olhadinha nas Postagens</Button>
-                  </Box>
-              </Grid>
-              <Grid item xs={6} >
-                  <img src="https://i.imgur.com/XOIrcHA.jpg" alt="imagem de uma mesa com intens da cor rosa" width="770px" height="750px"/>
-              </Grid>
-              <Grid xs={12} className="postagens">
-                <TabPostagem/>
-              </Grid>
-          </Grid>
-      </>
-  );
+
+    let navigate = useNavigate();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens);
+
+    useEffect(() => {
+        if (token == "") {
+        toast.error('Você precisa estar logado!',{
+            position:'top-right',
+            autoClose:2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,        
+        });
+        navigate("/login")
+        }
+    }, [token])
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Estou muito feliz em ver você por aqui!Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                            <ModalPostagem />
+                        </Box>
+                        <Link to="/posts" className="text-decorator-none">
+                            <Button variant="outlined" className='botao'>Da uma espiadinha nas minhas postagens!</Button>
+                        </Link>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://imgur.com/XOIrcHA" alt="" width="770px" height="750px" />
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                    <TabPostagem />
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default Home;
